c1d3bca50ebb01c327fe3dfde5529afc15cd6987
valmat <ufabiz@gmail.com>
2014-12-05 21:49:26 +0600

    Changed pre-file for tests Changed:tests/include/inc.php

--- tests/include/inc.php
+++ tests/include/inc.php
@@ -1,11 +1,11 @@
 <?php
 
-require dirname(__FILE__) . '/../../src/Rocks.php';
+require dirname(__FILE__) . '/../../src/Client.php';
 require dirname(__FILE__) . '/../../src/MgetIterator.php';
 require dirname(__FILE__) . '/../../src/Response.php';
 
 
-$db = new RocksServer\Client('localhost', 5533);
+$db = new RocksServer\RocksDB\Client('localhost', 5533);
 
 
 //echo var_dump(ScreenNav::pageNo('part')) .PHP_EOL;
